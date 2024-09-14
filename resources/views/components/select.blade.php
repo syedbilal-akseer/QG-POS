ba231d9c011a603d055f36de594cec36e51b8718
rashidali99 <rali@fossphorus.com>
2024-09-15 03:08:37 +0500

    WIP

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -6,8 +6,8 @@
 
 <select {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
     'class' =>
-        'py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-primary-500 focus:ring-primary-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600' .
-        ($isInvalid ? ' border-red-500' : ''),
+        'form-control py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-primary-500 focus:ring-primary-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600' .
+        ($isInvalid ? ' border-red-500 dark:border-red-500' : ''),
 ]) !!} name="{{ $name }}">
     {{ $slot }}
 </select>
