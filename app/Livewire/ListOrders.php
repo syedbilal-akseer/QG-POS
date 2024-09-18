424863b0757814db3f99af6328b20dca029d8047
rashidali99 <rali@fossphorus.com>
2024-09-19 01:43:49 +0500

    WIP

--- app/Livewire/ListOrders.php
+++ app/Livewire/ListOrders.php
@@ -145,66 +145,67 @@ class ListOrders extends Component implements HasForms, HasTable
             'orderItemWarehouses.*.required' => 'Warehouse must be selected for order item.',
         ]);
 
-        try {
-            $order = DB::connection('oracle')->transaction(function () {
-                // Create Oracle Order Header
-                $oracleOrderHeader = OracleOrderHeader::create([
-                    'order_source_id' => $this->order->order_number,
-                    'orig_sys_document_ref' => $this->order->order_number,
-                    'customer_id' => $this->order->customer->customer_id,
-                    'customer_name' => $this->order->customer->customer_name,
-                    'customer_number' => $this->order->customer->customer_number,
-                    'price_list_id' => $this->order->customer->price_list_id,
-                    'created_by' => auth()->user()->id,
-                    'ordered_date' => now(),
-                    'sold_to_org_id' => 1641,
-                    'creation_date' => now(),
-                    'order_source' => "POS",
-                    'last_updated_by' => auth()->user()->id,
-                    'last_update_date' => now(),
-                    'operation_code' => 'INSERT',
-                ]);
+        // try {
+        //     $order = DB::connection('oracle')->transaction(function () {
+        //         // Create Oracle Order Header
+        //         $oracleOrderHeader = OracleOrderHeader::create([
+        //             'order_source_id' => $this->order->order_number,
+        //             'orig_sys_document_ref' => $this->order->order_number,
+        //             'customer_id' => $this->order->customer->customer_id,
+        //             'customer_name' => $this->order->customer->customer_name,
+        //             'customer_number' => $this->order->customer->customer_number,
+        //             'price_list_id' => $this->order->customer->price_list_id,
+        //             'created_by' => auth()->user()->id,
+        //             'ordered_date' => now(),
+        //             'sold_to_org_id' => 1641,
+        //             'creation_date' => now(),
+        //             'order_source' => "POS",
+        //             'last_updated_by' => auth()->user()->id,
+        //             'last_update_date' => now(),
+        //             'operation_code' => 'INSERT',
+        //         ]);
 
-                // Create Oracle Order Lines
-                foreach ($this->order->orderItems as $index => $orderItem) {
-                    $selectedWarehouseId = $this->orderItemWarehouses[$index] ?? null;
+        //         // Create Oracle Order Lines
+        //         foreach ($this->order->orderItems as $index => $orderItem) {
+        //             $selectedWarehouseId = $this->orderItemWarehouses[$index] ?? null;
 
-                    // Update the local order item with the selected warehouse
-                    $orderItem->update(['warehouse_id' => $selectedWarehouseId]);
+        //             // Update the local order item with the selected warehouse
+        //             $orderItem->update(['warehouse_id' => $selectedWarehouseId]);
 
-                    OracleOrderLine::create([
-                        'order_source_id' => $oracleOrderHeader->id,
-                        'orig_sys_document_ref' => $this->order->order_number,
-                        'orig_sys_line_ref' => $this->order->order_number,
-                        'line_number' => $index,
-                        'inventory_item_id' => $orderItem->inventory_item_id,
-                        'ordered_quantity' => $orderItem->quantity,
-                        'org_id' => $selectedWarehouseId,
-                        'created_by' => auth()->user()->id,
-                        'creation_date' => now(),
-                        'last_updated_by' => auth()->user()->id,
-                        'last_update_date' => now(),
-                        'operation_code' => "INSERT",
-                    ]);
-                }
+        //             OracleOrderLine::create([
+        //                 'order_source_id' => $oracleOrderHeader->id,
+        //                 'orig_sys_document_ref' => $this->order->order_number,
+        //                 'orig_sys_line_ref' => $this->order->order_number,
+        //                 'line_number' => $index,
+        //                 'inventory_item_id' => $orderItem->inventory_item_id,
+        //                 'ordered_quantity' => $orderItem->quantity,
+        //                 'org_id' => $selectedWarehouseId,
+        //                 'created_by' => auth()->user()->id,
+        //                 'creation_date' => now(),
+        //                 'last_updated_by' => auth()->user()->id,
+        //                 'last_update_date' => now(),
+        //                 'operation_code' => "INSERT",
+        //             ]);
+        //         }
 
-                // Update the order's oracle_at timestamp to mark it as successfully entered into Oracle
-                $this->order->update(['oracle_at' => now()]);
+        //         // Update the order's oracle_at timestamp to mark it as successfully entered into Oracle
+        //         $this->order->update(['oracle_at' => now()]);
 
-                return $oracleOrderHeader;
-            });
+        //         return $oracleOrderHeader;
+        //     });
 
-            if ($order) {
-                $this->reset('order');
-                $this->dispatch('close');
-                $this->dispatch('toast-success', 'Order entered to Oracle successfully.');
-            } else {
-                throw new \Exception('Order insertion failed.');
-            }
-        } catch (\Exception $e) {
-            $this->dispatch('toast-error', 'An error occurred while entering the order to Oracle.');
-            // $this->dispatch('toast-error', 'An error occurred: ' . $e->getMessage());
-        }
+        //     if ($order) {
+        //         $this->reset('order');
+        //         $this->dispatch('close');
+        //         $this->dispatch('toast-success', 'Order entered to Oracle successfully.');
+        //     } else {
+        //         throw new \Exception('Order insertion failed.');
+        //     }
+        // } catch (\Exception $e) {
+        //     $this->dispatch('toast-error', 'An error occurred while entering the order to Oracle.');
+        //     // $this->dispatch('toast-error', 'An error occurred: ' . $e->getMessage());
+        // }
+        $this->dispatch('toast-error', 'This feature will not work on cPanel.');
     }
 
     public function render()
