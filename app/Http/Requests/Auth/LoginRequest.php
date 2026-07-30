7b8829339f6c9ae63f3ca21290ad3266c7c51679
mmansoor5565 <mmansoor5565@gmail.com>
2026-07-30 19:52:14 +0500

    inventory manager revert

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -52,17 +52,16 @@ class LoginRequest extends FormRequest
             ]);
         }
 
-        // dd($allowedRoles);
         // Fetch allowed roles dynamically from the roles table
         $allowedRoles = Cache::remember('allowed_login_roles', 3600, function () {
             return Role::pluck('name')->toArray();
         });
-        // dd($allowedRoles);
+
         // Check if the authenticated user has an allowed role
         $user = Auth::user();
         $userRole = $user->role_name;
 
-        if (!$userRole || !in_array($userRole, $allowedRoles) || !$userRole == 'inventory-manager') {
+        if (!$userRole || !in_array($userRole, $allowedRoles)) {
             Auth::logout();
 
             throw ValidationException::withMessages([
